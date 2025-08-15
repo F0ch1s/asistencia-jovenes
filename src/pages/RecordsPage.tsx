68d77becf75bcfc5cfc86b4bd1399496db6aa2ab
GDLM1208 <gllerena1208@gmail.com>
2025-08-15 18:52:49 -0500

    Funcionalidad y estilos para página de asistentes por evento

--- src/pages/RecordsPage.tsx
+++ src/pages/RecordsPage.tsx
@@ -1,38 +1,112 @@
 import { useEffect, useState } from "react";
 import supabase from "../lib/supabase";
-
+import '../styles/Events.css'
 interface Evento {
+  id: number;
   nombre: string;
   fecha: string;
 }
 
+interface Asistente {
+  id: number;
+  nombres: string;
+  apellidos: string;
+}
+
 const RecordsPage = () => {
-  const [eventos, setEventos] = useState<Evento[]>([{
-    nombre: '',
-    fecha: ''
-  }]);
+  const [eventos, setEventos] = useState<Evento[]>([]);
+  const [asistentes, setAsistentes] = useState<Asistente[]>([]);
+  const [eventoSeleccionado, setEventoSeleccionado] = useState<number | null>(null);
 
   const fetchEventos = async () => {
     const { data, error } = await supabase
       .from("eventos")
-      .select("nombre, fecha")
+      .select("id, nombre, fecha")
 
-    if (!error && data) setEventos(data);
+    if (!error && data) {
+      data.forEach((item) => {
+        //Cambiar formato AAAA-MM-DDTHH:mm:ss a DD-MM-AAAA
+        item.fecha = item.fecha.replace("T", " ");
+        const [fecha] = item.fecha.split(" ");
+        item.fecha = fecha;
+      })
+      setEventos(data);
+    }
   }
+
+  const fetchAsistentes = async (eventoId: number) => {
+    const { data, error } = await supabase
+    .from("eventos")
+    .select(`
+      id,
+      evento_asistentes (
+        asistentes (
+          id,
+          nombres,
+          apellidos
+          )
+          )
+          `)
+          .eq("id", eventoId)
+          .single();
+
+      if (!error && data) {
+        setAsistentes(
+          data.evento_asistentes.map((element: any) => element.asistentes) as Asistente[]
+        );
+      }
+  }
+
+  const handleClick = async (id: number) => {
+    if (eventoSeleccionado === id) {
+      // si ya está abierto → lo cerramos
+      setEventoSeleccionado(null);
+      setAsistentes([]);
+    } else {
+      setEventoSeleccionado(id);
+      await fetchAsistentes(id);
+    }
+  }
+
   useEffect(() => {
     fetchEventos()
   }, []);
 
   return (
-    <div>
+    <>
       <h1>Registros de asistentes</h1>
-      {eventos.map((item, index) => {
-        return <div key={index}>
-          <p>{item.nombre}</p>
-          <p>{item.fecha}</p>
-        </div>
-      })}
-    </div>
+            <div className="event-list">
+        {eventos.map((item) => (
+          <div key={item.id}>
+            <article
+              className="event-item"
+              onClick={() => handleClick(item.id)}
+            >
+              <p className="event-name">{item.nombre}</p>
+              <p className="event-date">{item.fecha}</p>
+            </article>
+
+            {/* Acordeón: solo se abre si el evento coincide */}
+            {eventoSeleccionado === item.id && (
+              <div className="accordion-content">
+                <h3>Asistentes:</h3>
+                {asistentes.length > 0 ? (
+                  <ul>
+                    {asistentes.map((a) => (
+                      <li key={a.id}>
+                        {a.nombres} {a.apellidos}
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>No hay asistentes registrados</p>
+                )}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
