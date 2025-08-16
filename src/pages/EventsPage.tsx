b6eff752aae679944f894c7aa0da75bafa21b9c4
Rodrigo Arenas <rodrigoarenas975@gmail.com>
2025-08-16 12:13:28 -0500

    Mejora de estilos de administrador y registro de eventos

--- src/pages/EventsPage.tsx
+++ src/pages/EventsPage.tsx
@@ -1,7 +1,14 @@
 import RegisterEventForm from "../components/RegisterEventForm";
+import "../styles/Events.css";
 
 const EventsPage = () => {
-    return <RegisterEventForm />
-}
+  return (
+    <div className="events-container">
+      <div className="events-card">
+        <RegisterEventForm />
+      </div>
+    </div>
+  );
+};
 
 export default EventsPage;
